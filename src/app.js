006f8956939d5f12fed437149a234081156b4d14
James Kelly <jk_jameskelly@outlook.com>
2017-06-03 11:32:11 -0400

    AsyncStorage

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Platform, ListView, Keyboard } from 'react-native';
+import { View, Text, StyleSheet, Platform, ListView, Keyboard, AsyncStorage } from 'react-native';
 import Header from './header';
 import Footer from './footer';
 import Row from './row';
@@ -39,13 +39,25 @@ class App extends Component {
     this.handleFilter = this.handleFilter.bind(this);
     this.handleClearComplete = this.handleClearComplete.bind(this);
   }
+
+  componentWillMount() {
+    AsyncStorage.getItem('items').then((json) => {
+      try {
+        const items = JSON.parse(json);
+        this.setSource(items, items);
+      } catch (e) {
+
+      }
+    })
+  }
   
   setSource(items, itemsDatasource, otherState = {}) {
     this.setState({
       items,
       dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
       ...otherState
-    })
+    });
+    AsyncStorage.setItem('items', JSON.stringify(items));
   }
 
   handleClearComplete() {
