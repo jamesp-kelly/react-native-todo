d762156c972a22a31707461e8007d07bc2510c4c
James Kelly <jk_jameskelly@outlook.com>
2017-06-03 12:02:06 -0400

    Inline editing

--- src/app.js
+++ src/app.js
@@ -39,6 +39,8 @@ class App extends Component {
     this.handleRemoveItem = this.handleRemoveItem.bind(this);
     this.handleFilter = this.handleFilter.bind(this);
     this.handleClearComplete = this.handleClearComplete.bind(this);
+    this.handleUpdateText = this.handleUpdateText.bind(this);
+    this.handleToggleEditing = this.handleToggleEditing.bind(this);
   }
 
   componentWillMount() {
@@ -94,6 +96,28 @@ class App extends Component {
     this.setSource(newItems, filterItems(this.state.filter, newItems));
   }
 
+  handleUpdateText(key, text) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item, 
+        text
+      };
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
+
+  handleToggleEditing(key, editing) {
+    const newItems = this.state.items.map((item) => {
+      if (item.key !== key) return item;
+      return {
+        ...item, 
+        editing
+      };
+    })
+    this.setSource(newItems, filterItems(this.state.filter, newItems));
+  }
+
   handleToggleComplete(key, complete) {
     const newItems = this.state.items.map((item) => {
       if (item.key !== key) return item;
@@ -135,6 +159,8 @@ class App extends Component {
               return (
                 <Row 
                   key={key}
+                  onUpdate={(text) => this.handleUpdateText(key, text)}
+                  onToggleEdit={(editing) => this.handleToggleEditing(key, editing)}
                   onComplete={(complete) => this.handleToggleComplete(key, complete)}
                   onRemove={() => this.handleRemoveItem(key)}
                   {...value}
--- src/row.js
+++ src/row.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet, Switch, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Switch, TouchableOpacity, TextInput } from 'react-native';
 
 class Row extends Component {
   render() {
-    const { text, complete, onComplete, onRemove } = this.props;
+    const { text, complete, onComplete, onRemove, onToggleEdit, editing } = this.props;
+
+    const textComponent = (
+      <TouchableOpacity style={styles.textWrap} onLongPress={() => onToggleEdit(true) }>
+        <Text style={[styles.text, complete && styles.complete]}>{text}</Text>
+      </TouchableOpacity>
+    );
+
+    const removeButton = (
+      <TouchableOpacity onPress={onRemove}>
+        <Text style={styles.destroy}>X</Text>
+      </TouchableOpacity>
+    );
+
+    const editingComponent = (
+      <View style={styles.textWrap}>
+        <TextInput 
+          onChangeText={this.props.onUpdate}
+          autoFocus
+          value={this.props.text}
+          style={styles.input}
+          multiline
+        />
+      </View>
+    );
+
+    const doneButton = (
+      <TouchableOpacity style={styles.done} onPress={() => this.props.onToggleEdit(false)}>
+        <Text style={styles.doneText}>Save</Text>
+      </TouchableOpacity>
+    );
+
     return (
       <View style={styles.container}>
         <Switch 
           value={complete}
           onValueChange={onComplete}  
         />
-        <View style={styles.textWrap}>
-          <Text style={[styles.text, complete && styles.complete]}>{text}</Text>
-        </View>
-        <TouchableOpacity onPress={onRemove}>
-          <Text style={styles.destroy}>X</Text>
-        </TouchableOpacity>
+
+        {editing ? editingComponent : textComponent}
+        {editing ? doneButton : removeButton}
+
       </View>
     );
   }
@@ -35,6 +64,23 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'flex-start',
     justifyContent: 'space-between'
+  },
+  input: {
+    height: 100,
+    flex: 1,
+    fontSize: 24,
+    padding: 0,
+    color: '#4d4d4d'
+  },
+  done: {
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#7be290',
+    padding: 7
+  },
+  doneText: {
+    color: '#4d4d4d',
+    fontSize: 20
   },
   textWrap: {
     flex: 1,
